0a204111a03e53917bbf0166f660482c068d2734
ihsangan <me@isan.eu.org>
2024-10-23 13:54:14 +0800

    PWA (manual)

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function() {});
+  });
+}
+let CACHE_NAME = "HTTPing";
+let urlsToCache = ["/"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+    return cache.addAll(urlsToCache);
+  }))
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response) {
+      return response;
+    }
+    return fetch(event.request);
+  }))
+});
+self.addEventListener("activate", (event) => {
+  let cacheWhitelist = ["HTTPing"];
+  event.waitUntil(caches.keys().then((cacheNames) => {
+    return Promise.all(cacheNames.map((cacheName) => {
+      if (cacheWhitelist.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName)
+      }
+    }))
+  }))
+})
